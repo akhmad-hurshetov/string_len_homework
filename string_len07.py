458aeecfdd3c9c760080723acd6a05a413d2a75e
Akhmad <akhmadhurshetov@gmail.com>
2022-11-07 06:26:02 +0500

    07

--- string_len07.py
+++ string_len07.py
@@ -8,12 +8,13 @@ def main(s1,s2,s3):
     Returns:
         string
     """
+    answer=0
     if len(s1)%2!=0:
         answer=f"{s1}"
         if len(s2)%2!=0:
-            answer=f"{s1}, {s2}"
+            answer=f"{s1, s2}"
             if len(s3)%2!=0:
-                answer=f"{s1}, {s2}, {s3}"
+                answer=f"{s1, s2, s3}"
     else:
         answer="[]"
     return answer
